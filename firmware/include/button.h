d5c617de74e22db75b71ad7509a83ab9ee3e0e9a
Volodymyr Zhdanov <wight554@gmail.com>
2026-05-01 17:01:27 +0300

    Remove unused button module

--- firmware/include/button.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include "hardware/gpio.h"
-
-typedef void (*gpio_irq_cb_t)(void *arg);
-
-void button_system_init(void);
-void listen(uint gpio, uint32_t events, gpio_irq_cb_t cb, void *arg);
